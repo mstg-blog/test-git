0851adab01655bdaec49cfe21063b6d7593b9089
wanyulaowang <1305949134@qq.com>
2024-04-17 12:25:37 +0800

    alter app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './'
+base_path = './test'
 os.system(f'git clone  https://code.openxlab.org.cn/wanyulaowang/test.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
